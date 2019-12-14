17deb73f6b8349ec35a36e738018d90f7067eba1
Kevin Nguy <nguy@Kevins-MacBook-Pro.local>
2019-12-13 23:20:30 -0800

    Update readme

--- README.sh
+++ README.sh
@@ -16,11 +16,13 @@ brew cask cleanup
 # Install zsh and plugins
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone git://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 # Copy zsh settings
 cp .zshrc ~/
 cp kevinnguy.zsh-theme ~/.oh-my-zsh/themes
+
+# Copy gitconfig
+cp .gitconfig ~/
 
 # install forked programs manually
 echo "Install forked version of Franz: https://github.com/kevinnguy/franz"
